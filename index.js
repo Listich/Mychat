687c3e185250ee7894047a1017244d185740f963
serena kifoula <serena.kifoula.eu>
2023-06-03 23:50:33 +0200

    Create a chat website using Firebase

---
+++ index.js
@@ -0,0 +1,23 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js';
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyD-_i7gnW4qqdz-py7RLLvv-BlbTp3dMXE",
+  
+    authDomain: "mychat-217fb.firebaseapp.com",
+  
+    databaseURL: "https://mychat-217fb-default-rtdb.europe-west1.firebasedatabase.app",
+  
+    projectId: "mychat-217fb",
+  
+    storageBucket: "mychat-217fb.appspot.com",
+  
+    messagingSenderId: "969350542768",
+  
+    appId: "1:969350542768:web:b2d479f17007ae7c9e9275",
+  
+    measurementId: "G-X7MP61J9X0"
+  
+  };
+  
+  const app = initializeApp(firebaseConfig);
